df5c09ca8c8e9b90f7647a7eccf27b54f911ff70
EduRomoGO <eduromogo@outlook.es>
2020-02-14 15:09:04 +0000

    feat: add avatar and health in fighter

--- src/components/Fighter/Fighter.js
+++ src/components/Fighter/Fighter.js
@@ -2,10 +2,19 @@ import React from 'react';
 import Dice from '../Dice/Dice';
 import './Fighter.css';
 
-const Fighter = () => {
+const Fighter = ({ type, currentHealth }) => {
+
+  const getAvatar = type => type === 'monster' ? '🧟‍♂️' : '🧙‍♂️';
+
   return <section className='c-fighter'>
-    <Dice />
-    <Dice />
+    <div className='c-fighter__dice-wrapper'>
+      <Dice />
+      <Dice />
+    </div>
+    <div className='c-fighter__health-avatar-wrapper'>
+      <div className='c-fighter__health'>{currentHealth}</div>
+      <div className='c-fighter__avatar'>{getAvatar(type)}</div>
+    </div>
   </section>
 }
 
