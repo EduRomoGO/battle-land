70ca3b41e5c327a51d46d8be40c7a3683a2177e2
EduRomoGO <eduromogo@outlook.es>
2020-02-14 15:06:26 +0000

    feat: set initial state and pass health to fighters

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Fighter from '../Fighter/Fighter';
 
+const initialHealth = 12;
+const initialState = {
+  monster: { health: initialHealth, attack: undefined },
+  character: { health: initialHealth, attack: undefined },
+};
+
+const getCurrentHealth = (type, state) => state[type].health;
+
 const Game = () => {
+  const [state, setState] = useState(initialState);
+
   return <section className='c-game'>
-    <Fighter />
-    <Fighter />
+    <Fighter type='character' currentHealth={getCurrentHealth('character', state)} />
+    <Fighter type='monster' currentHealth={getCurrentHealth('monster', state)} />
   </section>
 };
 
