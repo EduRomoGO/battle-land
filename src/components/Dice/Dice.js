9ca3bf405ff60f11bcc6e8a1cbdbff93f09971a5
EduRomoGO <eduromogo@outlook.es>
2020-02-14 14:45:46 +0000

    feat: add validations for wrong dice value

--- src/components/Dice/Dice.js
+++ src/components/Dice/Dice.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Dice = ({value}) => {
-  return <div>{value}</div>
+const Dice = ({ value = '?' }) => {
+  const isValidDiceNumber = value => parseInt(value, 10) <= 6 && parseInt(value, 10) >= 1;
+  const isValid = value => isValidDiceNumber(value) || value === '?';
+  const getValue = value => {
+    return isValid(value) ? value : 'An error occurred';
+  }
+
+  return <div className='c-dice component-wrapper'>
+      <div className='js-value'>{getValue(value)}</div>
+  </div>
 };
 
 export default Dice;
