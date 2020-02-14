36b2fbc793341fb1ec9de2d20425f408de96ca35
EduRomoGO <eduromogo@outlook.es>
2020-02-14 14:40:24 +0000

    feat: create Dice and add to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import './reset.css';
+import Dice from './components/Dice/Dice.js';
 
 function App() {
   return (
     <div className="App">
+      <Dice value={3} />
     </div>
   );
 }
