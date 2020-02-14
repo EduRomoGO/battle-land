1f1f50d09dcc91214ef4990d4d0227a28d782fbb
EduRomoGO <eduromogo@outlook.es>
2020-02-14 15:20:59 +0000

    feat: handle attack click

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,12 +7,51 @@ const initialState = {
   character: { health: initialHealth, attack: undefined },
 };
 
+const dice = [1, 2, 3, 4, 5, 6];
+const getDiceFace = () => Math.floor(Math.random() * dice.length) + 1;
+
 const getCurrentHealth = (type, state) => state[type].health;
+
+const getAttackPoints = fighter => fighter.attack && fighter.attack.reduce((acc, next) => acc + next, 0);
+
+function getDmgResults({ monster, character }) {
+  const monsterAttack = getAttackPoints(monster);
+  const characterAttack = getAttackPoints(character);
+
+  return {
+    monster: Math.max(characterAttack - monsterAttack, 0),
+    character: Math.max(monsterAttack - characterAttack, 0),
+  };
+};
 
 const Game = () => {
   const [state, setState] = useState(initialState);
 
+
+  const handleAttackClick = () => {
+    const getNewRound = state => {
+      const newState = {
+        monster: { attack: [getDiceFace(), getDiceFace()] },
+        character: { attack: [getDiceFace(), getDiceFace()] },
+      };
+      const dmgResults = getDmgResults(newState);
+
+      newState.character.health = Math.max(state.character.health - dmgResults.character, 0);
+      newState.monster.health = Math.max(state.monster.health - dmgResults.monster, 0);
+
+      return newState;
+    };
+
+    setState(state => getNewRound(state));
+  };
+
+
+  const renderAttackButton = () => {
+    return <button onClick={handleAttackClick}>Attack</button>;
+  };
+
   return <section className='c-game'>
+    <div className='c-game__actions'>{renderAttackButton()}</div>
     <Fighter type='character' currentHealth={getCurrentHealth('character', state)} />
     <Fighter type='monster' currentHealth={getCurrentHealth('monster', state)} />
   </section>
