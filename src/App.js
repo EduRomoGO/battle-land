e54cc45b8446d385fc7c4726c181c9f407e0e828
EduRomoGO <eduromogo@outlook.es>
2020-02-14 14:52:05 +0000

    feat: fighter

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import './reset.css';
 import Dice from './components/Dice/Dice.js';
+import Fighter from './components/Fighter/Fighter.js';
 
 function App() {
   return (
     <div className="App">
+      <Fighter />
       <Dice value={3} />
     </div>
   );
---
+++ src/components/Fighter/Fighter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Fighter = () => {
+  return <section className='c-fighter'>
+Fighter
+  </section>
+}
+
+export default Fighter;
