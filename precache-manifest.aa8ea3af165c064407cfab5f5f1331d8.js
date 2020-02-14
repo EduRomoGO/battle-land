2ce754da4a98f8eb665be49bb1b702ac9081887b
EduRomoGO <eduromogo@outlook.es>
2020-02-14 22:40:59 +0000

    Updates

---
+++ precache-manifest.aa8ea3af165c064407cfab5f5f1331d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469d0cdc66f8466430f2aaf2b930aa69",
+    "url": "/battle-land/index.html"
+  },
+  {
+    "revision": "1c02bd1fb5093962ae52",
+    "url": "/battle-land/static/css/main.8a77eaa3.chunk.css"
+  },
+  {
+    "revision": "f2b043aed7615b4f762a",
+    "url": "/battle-land/static/js/2.65a417da.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/battle-land/static/js/2.65a417da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c02bd1fb5093962ae52",
+    "url": "/battle-land/static/js/main.176ea183.chunk.js"
+  },
+  {
+    "revision": "cc888c074dc25284a16c",
+    "url": "/battle-land/static/js/runtime-main.1237385c.js"
+  },
+  {
+    "revision": "3fcfe53c8157f72a090f3d990e89187c",
+    "url": "/battle-land/static/media/Health.3fcfe53c.svg"
+  }
+]);
